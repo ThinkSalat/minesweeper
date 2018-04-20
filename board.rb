1fa6de18697bf3422f67bbe7c48d4d25b6acc04c
Shawn Salat <Shawn.salat@gmail.com>
2018-04-19 17:28:19 -0700

    Add populate and add_bombs methods to board.rb

--- board.rb
+++ board.rb
@@ -1,8 +1,26 @@
 class Board
-  def initialize
+  def initialize(bombcount=8)
+    @bombcount = bombcount
     @grid = Array.new(10) { Array.new(10) }
   end
 
-  
+  def populate
+    @grid.flatten.each do |cell|
+      cell << Tile.new(self)
+    end
+    add_bombs
+  end
+  def add_bombs
+    bomb_count = 0
+    until bomb_count == @bombcount
+      current_tile = @grid.flatten.sample
+      next if current_tile.value == :b
+      current_tile.value = :b
+      bomb_count += 1
+    end
+  end
 
+  def render
+    
+  end
 end
