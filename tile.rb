5e164f7a70d8a9671a1b93f69107a92150a776ce
Shawn Salat <Shawn.salat@gmail.com>
2018-04-19 17:19:00 -0700

    Add initialization to tile class

--- tile.rb
+++ tile.rb
@@ -1,3 +1,20 @@
+TILE_VALUES = [:b,:e]
 class Tile
+  attr_accessor :value, :revealed
+  def initialize(board)
+    @value = :e
+    @revealed = false
+    @board = board
+    @flagged = false
+  end
+
+  def reveal
+    raise "Tile is flagged" if @flagged
+    @revealed = !@revealed
+  end
+
+  def revealed?
+    @revealed
+  end
 
 end
